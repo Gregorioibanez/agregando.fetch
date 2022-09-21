d49b32dee9c8107afd7cd3cd122364ad73166b20
Gregorio Ibanez <gregoibanez13@gmail.com>
2022-09-21 14:23:32 -0300

    probando trabajo final

--- js/array.js
+++ js/array.js
@@ -1,26 +1,29 @@
-const Estilos = [
-    {
+const Estilos = [{
         id: 100,
         nombre: "Estucado Veneciano",
         precio: "$2500",
-        stock: "1000"
+        stock: "1000",
+        imagen: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F550x%2Fc2%2Fed%2Fd0%2Fc2edd0f88f3c25e6ab20f9701e7e96aa.jpg&imgrefurl=https%3A%2F%2Fco.pinterest.com%2Fheli_villamizar%2Festuco-veneciano%2F&tbnid=EwfNOlK3oGYt7M&vet=12ahUKEwjZ8Yqw-aX6AhWFuJUCHYoxCogQMygJegUIARDzAQ..i&docid=p08DJOL78j61rM&w=549&h=412&q=estucado%20&ved=2ahUKEwjZ8Yqw-aX6AhWFuJUCHYoxCogQMygJegUIARDzAQ"
     },
     {
         id: 103,
         nombre: "Estucado Clasico",
         precio: "$1800",
-        stock: "1500"
+        stock: "1500",
+        imagen: ""
     },
     {
         id: 107,
         nombre: "Microcemento alisado",
         precio: "$2000",
-        stock: "1800"
+        stock: "1800",
+        imagen: ""
     },
     {
         id: 110,
         nombre: "Estucado en mesadas, muebles",
         precio: "$3000",
-        stock: "500"
+        stock: "500",
+        imagen: ""
     }
 ]
